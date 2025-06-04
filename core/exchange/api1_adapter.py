01c3551e0050735f51c555551f8d05b3d8182fac
m41k1 <michalhinojosa@gmail.com>
2025-06-04 18:31:59 -0500

    feat: bruno para transactions

--- core/exchange/api1_adapter.py
+++ core/exchange/api1_adapter.py
@@ -3,6 +3,8 @@ from typing import Dict
 from .interface import ExchangeAPIInterface
 from config import EXCHANGE_API1_KEY, SUPPORTED_CURRENCIES
 
+print("key: ", EXCHANGE_API1_KEY)
+
 class ExchangeRateAPI(ExchangeAPIInterface):
     """Adaptador para ExchangeRate-API"""
     
