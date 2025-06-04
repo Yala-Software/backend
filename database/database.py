b51ce8bc74132b7c678833e1930ddce5375692b0
iankev <iankevin.cf@gmail.com>
2025-06-03 23:39:45 -0500

    All done

--- database/database.py
+++ database/database.py
@@ -40,7 +40,7 @@ def init_db_data():
         
         user_x = User(
             username="X",
-            email="ian.6780.cf@gmail.com",
+            email="x@example.com",
             hashed_password=hashed_password,
             full_name="User X"
         )
